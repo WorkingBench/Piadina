90c14d3040a3e9dcb17060d8c7b264cf3ac73545
Andrei-Ionut Camarasan <andrei.camarasan@pitechnologies.ro>
2016-01-11 17:03:44 +0200

    Google maps feature.

--- src/Picola/PiadinaBundle/Resources/public/js/main.js
+++ src/Picola/PiadinaBundle/Resources/public/js/main.js
@@ -94,5 +94,17 @@ $(document).ready(function() {
 		);
 	});
 
+//    Initialize Google Maps
+
+    function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(46.767514, 23.587197),
+            zoom: 18,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 });
